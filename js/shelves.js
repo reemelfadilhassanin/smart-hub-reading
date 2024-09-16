c5aa9bbe3d19bee1d9bd75e0494819716ffbbbce
reemelfadilhassanin <141276742+reemelfadilhassanin@users.noreply.github.com>
2024-09-16 10:46:08 +0300

    complete favorit page

--- js/shelves.js
+++ js/shelves.js
@@ -21,15 +21,15 @@ $(document).ready(function() {
     function renderShelves() {
         for (var shelf in books) {
             var shelfId = "#" + shelf;
-    
+
             if (!Array.isArray(books[shelf])) {
                 console.error(`Expected an array for shelf: ${shelf}. Initializing as empty array.`);
                 books[shelf] = [];
             }
-    
+
             $(shelfId).empty();
             var seenBooks = new Set();
-    
+
             books[shelf].forEach(function(book) {
                 var bookKey = `${book.title} by ${book.author}`;
                 if (!seenBooks.has(bookKey)) {
@@ -38,15 +38,14 @@ $(document).ready(function() {
                     var isFavorite = favorites.some(fav => fav.title === book.title && fav.author === book.author);
                     var heartButtonText = isFavorite ? '❤︎' : '♡';
                     var heartButtonTitle = isFavorite ? 'Remove from favorites' : 'Add to favorites';
-    
-                    // Update the tooltipContent to include start date and end date
+
                     var tooltipContent = `
                         <strong>Comment:</strong> ${book.comment || 'No comment'}<br>
                         <strong>Start Date:</strong> ${book.startDate || 'Not specified'}<br>
                         <strong>End Date:</strong> ${book.endDate || 'Not specified'}<br>
                         <strong>Pages:</strong> ${book.pages || 'Not specified'}
                     `;
-    
+
                     $(shelfId).append(
                         `<li class="shelf-list-item">
                             <img src="${book.coverImage || 'path/to/default-cover.jpg'}" alt="${book.title}" class="book-cover-img">
@@ -65,18 +64,54 @@ $(document).ready(function() {
                     seenBooks.add(bookKey);
                 }
             });
-    
+
             if (shelf === "read" && books[shelf].length === 0) {
                 $("#read-empty-message").show();
             } else {
                 $("#read-empty-message").hide();
             }
         }
-    
+
         // Re-initialize tooltips after dynamic content is added
         $('[data-toggle="tooltip"]').tooltip();
     }
-    
+
+    function renderFavorites() {
+        var favoritesList = $("#favorites-list");
+        favoritesList.empty();
+
+        favorites.forEach(function(favorite) {
+            var book = books['currently-reading'].concat(books['want-to-read'], books['read'])
+                .find(b => b.title === favorite.title && b.author === favorite.author);
+
+            if (book) {
+                var tooltipContent = `
+                    <strong>Comment:</strong> ${book.comment || 'No comment'}<br>
+                    <strong>Start Date:</strong> ${book.startDate || 'Not specified'}<br>
+                    <strong>End Date:</strong> ${book.endDate || 'Not specified'}<br>
+                    <strong>Pages:</strong> ${book.pages || 'Not specified'}
+                `;
+
+                favoritesList.append(
+                    `<li class="shelf-list-item">
+                        <img src="${book.coverImage || 'path/to/default-cover.jpg'}" alt="${book.title}" class="book-cover-img">
+                        <div class="book-title-container">
+                            <span class="book-title">${book.title} by ${book.author}</span>
+                            <button class="btn btn-sm btn-primary comment-button" data-title="${book.title}" data-author="${book.author}" data-toggle="tooltip" data-html="true" title="${tooltipContent}">
+                                <i class="fas fa-comment-dots"></i>
+                            </button>
+                        </div>
+                        <button class="btn-heart liked" data-title="${book.title}" data-author="${book.author}" title="Remove from favorites">
+                            ❤︎
+                        </button>
+                    </li>`
+                );
+            }
+        });
+
+        // Re-initialize tooltips after dynamic content is added
+        $('[data-toggle="tooltip"]').tooltip();
+    }
 
     function removeBookFromAllShelves(bookTitle, bookAuthor) {
         for (var shelf in books) {
@@ -86,6 +121,7 @@ $(document).ready(function() {
         }
         favorites = favorites.filter(fav => !(fav.title === bookTitle && fav.author === bookAuthor));
         localStorage.setItem('favorites', JSON.stringify(favorites));
+        localStorage.setItem('books', JSON.stringify(books));
     }
 
     function moveToNextShelf(bookTitle, bookAuthor, currentShelf) {
@@ -103,6 +139,7 @@ $(document).ready(function() {
             }
             localStorage.setItem('books', JSON.stringify(books));
             renderShelves();
+            renderFavorites();
         }
     }
 
@@ -119,6 +156,7 @@ $(document).ready(function() {
             books[shelf] = [];
             localStorage.setItem('books', JSON.stringify(books));
             renderShelves();
+            renderFavorites();
         } else {
             alert("Invalid shelf name!");
         }
@@ -141,7 +179,7 @@ $(document).ready(function() {
 
         localStorage.setItem('favorites', JSON.stringify(favorites));
         renderShelves();
-        renderRecommendations();
+        renderFavorites();
     });
 
     $(document).on('click', '.comment-button', function() {
@@ -151,90 +189,32 @@ $(document).ready(function() {
             .find(b => b.title === title && b.author === author);
 
         $('#comment').val(book ? book.comment || '' : '');
-        $('#start-date').val(book ? book.startDate || '' : ''); // Change to startDate
-        $('#end-date').val(book ? book.endDate || '' : ''); // Change to endDate
-        $('#pages').val(book ? book.pages || '0' : '0'); // Set default to 0
-        $('#pages-slider').val(book ? book.pages || '0' : '0'); // Sync slider with pages
-        $('#commentModal').data('title', title).data('author', author).modal('show');
-    });
-
-    $('#pages-slider').on('input', function() {
-        $('#pages').val($(this).val());
-    });
-
-    $('#pages').on('input', function() {
-        var value = $(this).val();
-        $('#pages-slider').val(value);
+        $('#start-date').val(book ? book.startDate || '' : '');
+        $('#end-date').val(book ? book.endDate || '' : '');
+        $('#pages').val(book ? book.pages || '0' : '');
+        $('#save-comment').data('title', title).data('author', author);
     });
 
     $('#save-comment').click(function() {
-        var title = $('#commentModal').data('title');
-        var author = $('#commentModal').data('author');
-        var comment = $('#comment').val();
-        var startDate = $('#start-date').val(); // Use startDate instead of date
-        var endDate = $('#end-date').val(); // Use endDate instead of date
-        var pages = $('#pages').val();
+        var title = $(this).data('title');
+        var author = $(this).data('author');
+        var book = books['currently-reading'].concat(books['want-to-read'], books['read'])
+            .find(b => b.title === title && b.author === author);
 
-        updateBookInfo(title, author, { comment, startDate, endDate, pages });
-        $('#commentModal').modal('hide');
-        renderShelves();
+        if (book) {
+            book.comment = $('#comment').val();
+            book.startDate = $('#start-date').val();
+            book.endDate = $('#end-date').val();
+            book.pages = $('#pages').val();
+            localStorage.setItem('books', JSON.stringify(books));
+            renderShelves();
+            renderFavorites();
+        }
     });
 
-    function updateBookInfo(title, author, info) {
-        books = Object.keys(books).reduce((acc, shelf) => {
-            acc[shelf] = books[shelf].map(book => {
-                if (book.title === title && book.author === author) {
-                    return { ...book, ...info };
-                }
-                return book;
-            });
-            return acc;
-        }, {});
-
-        localStorage.setItem('books', JSON.stringify(books));
-    }
-
-    function renderRecommendations() {
-        var recommendedBooks = $('#recommended-books');
-        recommendedBooks.empty();
-
-        var recommended = [];
-
-        var readBooks = books['read'] || [];
-        readBooks.forEach(function(readBook) {
-            var currentlyReading = books['currently-reading'] || [];
-            var wantToRead = books['want-to-read'] || [];
-
-            currentlyReading.forEach(function(book) {
-                if (book.title !== readBook.title && book.author === readBook.author) {
-                    recommended.push(book);
-                }
-            });
-            wantToRead.forEach(function(book) {
-                if (book.title !== readBook.title && book.author === readBook.author) {
-                    recommended.push(book);
-                }
-            });
-        });
-
-        recommended = Array.from(new Set(recommended.map(book => `${book.title}${book.author}`)))
-            .map(key => recommended.find(book => `${book.title}${book.author}` === key))
-            .slice(0, 8);
-
-        recommended.forEach(function(book) {
-            recommendedBooks.append(
-                `<div class="col-md-3 mb-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">${book.title}</h5>
-                            <p class="card-text">${book.author}</p>
-                        </div>
-                    </div>
-                </div>`
-            );
-        });
-    }
+    // Initialize tooltips
+    $('[data-toggle="tooltip"]').tooltip();
 
     renderShelves();
-    renderRecommendations();
+    renderFavorites();
 });
